8cb9a22295fb1ca9aeb9b4dacc718e93ef9b4f45
barbosathiago <thiago.barbosa@infouniron.com>
2017-12-31 05:11:14 -0400

    Refactor dos serviços para HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import {ROUTES} from './app.routes'
 import {RouterModule} from '@angular/router'
 import { AppComponent } from './app.component';
@@ -22,7 +22,7 @@ import { TextMaskModule } from 'angular2-text-mask';
   ],
   imports: [
     TextMaskModule,
-    HttpModule,
+    HttpClientModule,
     BrowserModule,
     RouterModule.forRoot(ROUTES),
     SharedModule.forRoot(),
--- src/app/shared/services/ocorrencias.service.ts
+++ src/app/shared/services/ocorrencias.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core'
 import {Http, Headers, RequestOptions} from '@angular/http'
+import {HttpClient, HttpParams} from '@angular/common/http'
 
 import {Observable} from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
@@ -13,87 +14,73 @@ import {ErrorHandler} from '../app.error-handler'
 
 @Injectable()
 export class OcorrenciasService {
-  constructor(private http: Http){}
+  constructor(private http: HttpClient){}
 
   getTwoOcorrencias(id1: string, id2: string):Observable<Ocorrencia[]>{
     return Observable.forkJoin(
-      this.http.get(`${CAVEIRINHA_API}/ocorrencias/${id1}`).map(reponse => reponse.json()).catch(ErrorHandler.handleError),
-      this.http.get(`${CAVEIRINHA_API}/ocorrencias/${id1}`).map(reponse => reponse.json()).catch(ErrorHandler.handleError)
+      this.http.get(`${CAVEIRINHA_API}/ocorrencias/${id1}`).catch(ErrorHandler.handleError),
+      this.http.get(`${CAVEIRINHA_API}/ocorrencias/${id1}`).catch(ErrorHandler.handleError)
     )
   }
 
   registraOcorrencia(ocorrencia: Ocorrencia):Observable<string>{
-    const headers = new Headers()
-    headers.append('Content-Type', 'application/json')
-    return this.http.post(`${CAVEIRINHA_API}/ocorrencias`,
-                          JSON.stringify(ocorrencia),
-                          new RequestOptions({headers: headers})
-                        ).map(response => response.json())
+    return this.http.post<string>(`${CAVEIRINHA_API}/ocorrencias`,ocorrencia)
                         .map(message => message['message'])
   }
 
-  ocorrencias():Observable<Ocorrencia[]>{
-    return this.http.get(`${CAVEIRINHA_API}/ocorrencias`)
-      .map(response => {
-        var result = response.json()
-        console.log(result)
-        return result
-      })
+  ocorrencias(placa?: string, numeroMotor?:string, chassis?: string, situacao?:string):Observable<Ocorrencia[]>{
+    placa = placa ? placa : ''
+    numeroMotor = numeroMotor ? numeroMotor : ''
+    chassis = chassis ? chassis : ''
+    situacao = situacao ? situacao : ''    
+    let params: HttpParams = undefined
+    params = new HttpParams().set('placa', placa)
+                            .set('numeroMotor', numeroMotor)
+                            .set('chassis', chassis)
+                            .set('situacao', situacao)
+    return this.http.get<Ocorrencia[]>(`${CAVEIRINHA_API}/ocorrencias`, {params: params})
       .catch(ErrorHandler.handleError)
   }
 
   ocorrenciasById(id: number): Observable<Ocorrencia>{
     return this.http.get(`${CAVEIRINHA_API}/ocorrencias/${id}`)
-      .map(reponse => reponse.json())
       .catch(ErrorHandler.handleError)
   }
 
   ocorrenciasOfVeiculo(id: number): Observable<Ocorrencia[]>{
     return this.http.get(`${CAVEIRINHA_API}/veiculo/${id}/ocorrencias`)
-      .map(response => response.json())
       .catch(ErrorHandler.handleError)
   }
 
   ocorrenciasOfDp(id: number): Observable<Ocorrencia[]>{
     return this.http.get(`${CAVEIRINHA_API}/dp/${id}/ocorrencias`)
-      .map(reponse => reponse.json())
       .catch(ErrorHandler.handleError)
   }
 
   veiculos(id: number): Observable<Veiculo[]>{
       return this.http.get(`${CAVEIRINHA_API}/veiculo/`)
-      .map(response => response.json())
       .catch(ErrorHandler.handleError)
   }
 
   registraVeiculo(veiculo: Veiculo): Observable<string>{
-    const headers = new Headers()
-    headers.append('Content-Type', 'application/json')
-
-    return this.http.post(`${CAVEIRINHA_API}/veiculos`,
-      JSON.stringify(veiculo),
-      new RequestOptions({headers: headers})
-    ).map(response => response.json()['public_id'])
+    return this.http.post<string>(`${CAVEIRINHA_API}/veiculos`, veiculo).map(response => response['public_id'])
     .catch(ErrorHandler.handleError)
   }
 
   veiculoById(id: number): Observable<Veiculo>{
     return this.http.get(`${CAVEIRINHA_API}/veiculo/${id}`)
-      .map(reponse => reponse.json())
       .catch(ErrorHandler.handleError)
   }
 
   veiculoOfOcorrencia(id: number): Observable<Veiculo>{
     return this.http.get(`${CAVEIRINHA_API}/ocorrencias/${id}/veiculo`)
-      .map(reponse => reponse.json())
       .catch(ErrorHandler.handleError)
   }
 
   getAllDps():Observable<Dp[]>{
-    return this.http.get(`${CAVEIRINHA_API}/dps`)
+    return this.http.get<Dp[]>(`${CAVEIRINHA_API}/dps`)
       .map(response => {
-        var result = response.json()['dps']
-        console.log(result)
+        var result = response['dps']
         return result
       })
       .catch(ErrorHandler.handleError)
@@ -101,7 +88,6 @@ export class OcorrenciasService {
 
   dpById(id: number): Observable<Dp>{
     return this.http.get(`${CAVEIRINHA_API}/dp/${id}`)
-      .map(response => response.json())
       .catch(ErrorHandler.handleError)
   }
 
