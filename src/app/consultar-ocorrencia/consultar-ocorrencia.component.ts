a1fdb0a5de4e9530147df3ae7de70d478741a51c
barbosathiago <thiago.barbosa@infouniron.com>
2018-01-01 23:00:09 -0400

    Implementadas ações de Conclusão e Re-abertura de ocorrências, assim como uma mudança visual na situação da ocorrencia baseado no valor da mesma

--- src/app/consultar-ocorrencia/consultar-ocorrencia.component.ts
+++ src/app/consultar-ocorrencia/consultar-ocorrencia.component.ts
@@ -1,25 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { OcorrenciasService } from '../shared/services/ocorrencias.service'
 import {Ocorrencia} from '../shared/models/ocorrencia.model'
+import { DataTableDirective } from 'angular-datatables';
+import { Subject } from 'rxjs/Subject';
+
 @Component({
   selector: 'cav-consultar-ocorrencia',
   templateUrl: './consultar-ocorrencia.component.html',
 })
-export class ConsultarOcorrenciaComponent implements OnInit {
+export class ConsultarOcorrenciaComponent implements OnInit, AfterViewInit {
 
-  ocorrencias: Ocorrencia[]
+  // DataTable properties
+  @ViewChild(DataTableDirective)
+  dtElement: DataTableDirective;
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
+
+  // Component Properties
+  ocorrencias = []
   searched: boolean = false
+
 
   constructor(private ocorrenciasService: OcorrenciasService) { }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit(){
+    this.dtTrigger.next()
   }
 
   pesquisarOcorrencias(placa, motor, chassis){
     if (!this.searched) {
       this.searched = true
     }
-    this.ocorrenciasService.ocorrencias(placa, motor, chassis, '').subscribe(result => this.ocorrencias = result['ocorrencias'])
+    this.ocorrenciasService.ocorrencias(placa, motor, chassis, '').subscribe(result => {
+      this.ocorrencias = result['ocorrencias']
+      this.rerender()
+    })
+
+
+  }
+
+  concluirOcorrencia(ocorrencia, action: string){
+    let idOcorrencia = ocorrencia.public_id
+    this.ocorrenciasService.ocorrenciasById(idOcorrencia).subscribe(result => {
+      result = result['ocorrencia']
+      result.situacao = action === 'C'? 'CONCLUÍDA': 'PENDENTE'
+      this.ocorrenciasService.updateOcorrencia(idOcorrencia, result).subscribe(response => {
+        console.log()
+        this.ocorrencias[this.ocorrencias.indexOf(ocorrencia)] = result
+        this.rerender()
+
+      })
+    })
+  }
+
+  rerender(): void {
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      // Destroy the table first
+      dtInstance.destroy();
+      // Call the dtTrigger to rerender again
+      this.dtTrigger.next();
+    });
   }
 
 }
