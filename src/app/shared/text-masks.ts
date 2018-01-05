8f2477f2a3b363be32ed4e9d57619976154b14c4
barbosathiago <thiago.barbosa@infouniron.com>
2018-01-05 00:32:50 -0400

    Corrigido um bug das masks onde não estava sendo considerado o numero 0

--- src/app/shared/text-masks.ts
+++ src/app/shared/text-masks.ts
@@ -1,7 +1,7 @@
 const placaP =[/[A-Z]/i,/[A-Z]/i,/[A-Z]/i,'-',/\d/,/\d/,/\d/,/\d/]
-const numeroCasaP=[/[1-9]/,/[1-9]/,/[1-9]/,/[1-9]/,]
-const anoVeiculoP=[/[1-9]/,/[1-9]/,/[1-9]/,/[1-9]/,]
-const numeroOcorrenciaP=[/[1-9]/,/[1-9]/,/[1-9]/,/[1-9]/,/[1-9]/,/[1-9]/,'/',/[1-9]/,/[1-9]/,/[1-9]/,/[1-9]/,]
+const numeroCasaP=[/[0-9]/,/[0-9]/,/[0-9]/,/[0-9]/,]
+const anoVeiculoP=[/[0-9]/,/[0-9]/,/[0-9]/,/[0-9]/,]
+const numeroOcorrenciaP=[/[0-9]/,/[0-9]/,/[0-9]/,/[0-9]/,/[0-9]/,/[0-9]/,'/',/[0-9]/,/[0-9]/,/[0-9]/,/[0-9]/,]
 const chassisP=[/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,/\w/,]
 
 export{placaP, numeroCasaP, chassisP,anoVeiculoP, numeroOcorrenciaP}
