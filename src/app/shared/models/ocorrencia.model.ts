a3d44736d185f14724fd5b0c5488612844190c84
barbosathiago <thiago.barbosa@infouniron.com>
2018-01-21 07:17:23 -0400

    Atualização das models para refletirem o novo formato de dados da api

--- src/app/shared/models/ocorrencia.model.ts
+++ src/app/shared/models/ocorrencia.model.ts
@@ -1,44 +1,49 @@
 class Ocorrencia {
   /** This is a description of the foo function. */
-  constructor(
-    public localOcorrencia: string,
-    public numeroOcorrencia: string,
-    public dp: Dp,
-    public tipo: string,
-    public situacao: string = 'PENDENTE',
-    public veiculo: Veiculo,
-    public data: Date,
-    public observacoes?: string,
-    public public_id?: string
-  ){}
+  constructor(){
+    this.situacao = "PENDENTE"
+  }
+  public data: Date;
+  public dp_id: string;
+  public veiculo_id: string;
+  public localOcorrencia: string;
+  public numeroOcorrencia: string;
+  public observacoes: string;
+  public situacao: string = 'PENDENTE';
+  public tipo: string;
+  public veiculo: Veiculo;
+  public dp: Dp;
+  public id: string;
 }
 
 class Veiculo {
     constructor(
-      public placa: string,
-      public tipo: string,
-      public proprietario: Proprietario,
-      public chassis?: string,
-      public numeroMotor?: string,
-      public ano?: string,
-      public cor?: string,
-      public public_id?: string
     ){}
+
+    public ano: string;
+    public chassis: string;
+    public cor: string;
+    public numeroMotor: string;
+    public placa: string;
+    public proprietario_id: string;
+    public tipo: string;
+    public proprietario: Proprietario;
+    public id: string;
 }
 
 class Dp {
   constructor(
-    public public_id: string = '',
-    public nome: string
   ){}
+  public id: string;
+  public nome: string;
 }
 
 class Proprietario{
   constructor(
-    public nome: string,
-    public contato: string,
-    public public_id?: string
   ){}
+  public nome: string;
+  public contato: string;
+  public id: string;
 }
 
 export {Ocorrencia, Dp, Veiculo, Proprietario}
